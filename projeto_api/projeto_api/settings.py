7cea17d7b11150bb56af77c8ef7d5ab378ae51ff
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-04-10 13:38:55 -0300

    Final do ´projeto

--- projeto_api/projeto_api/settings.py
+++ projeto_api/projeto_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'produtos',
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'projeto_api.urls'
@@ -122,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500", # Exemplo para um servidor de desenvolvimento HTML
+    "http://seu-dominio-frontend.com",
+    # Adicione outras origens conforme necessário
+]
